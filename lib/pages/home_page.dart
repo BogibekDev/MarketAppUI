8b5d4b12e4f0481149af0a8209bffd0fa84815db
bogibekdev <bogibekdev@gmail.com>
2023-05-05 12:08:52 +0500

    finish

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +11,198 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Text(
+          "Robots",
+          style: TextStyle(color: Colors.black, fontSize: 25),
+        ),
+        centerTitle: false,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.notifications_none,
+              color: Colors.black,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.shopping_cart,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              //#category
+              SizedBox(
+                height: 40,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    singleTab(isSelected: true, title: "All"),
+                    singleTab(isSelected: false, title: "Red"),
+                    singleTab(isSelected: false, title: "Blue"),
+                    singleTab(isSelected: false, title: "Green"),
+                    singleTab(isSelected: false, title: "Yellow"),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              //#Items
+              item(
+                  image: "assets/images/img.png",
+                  title: "WAIRER AI",
+                  type: "Barakali AI",
+                  price: 100),
+
+              item(
+                  image: "assets/images/img_1.png",
+                  title: "WAIRER AI",
+                  type: "Barakali AI",
+                  price: 75),
+
+              item(
+                  image: "assets/images/img_2.png",
+                  title: "WAIRER AI",
+                  type: "Barakali AI",
+                  price: 90),
+
+              item(
+                  image: "assets/images/img_3.png",
+                  title: "WAIRER AI",
+                  type: "Barakali AI",
+                  price: 10),
+
+              item(
+                  image: "assets/images/img_5.png",
+                  title: "WAIRER AI",
+                  type: "Barakali AI",
+                  price: 30),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget singleTab({isSelected, title}) {
+    return AspectRatio(
+      aspectRatio: 2.2 / 1,
+      child: Container(
+        margin: const EdgeInsets.only(right: 10),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.grey.shade200 : Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: isSelected ? 20 : 17,
+              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget item({image, title, type, price}) {
+    return Container(
+      height: 250,
+      width: MediaQuery.of(context).size.width,
+      margin: const EdgeInsets.only(bottom: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(
+          image: AssetImage(image),
+          fit: BoxFit.cover,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade400,
+            blurRadius: 10,
+            offset: const Offset(0, 10),
+          ),
+        ],
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            colors: [
+              Colors.black.withOpacity(0.9),
+              Colors.black.withOpacity(0.6),
+              Colors.black.withOpacity(0.3),
+              Colors.black.withOpacity(0.2),
+            ],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        type,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 35,
+                  height: 35,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: const Center(
+                    child: Icon(
+                      Icons.favorite_border,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              '\$$price',
+              style: const TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
